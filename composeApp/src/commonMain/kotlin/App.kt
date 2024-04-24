13b1e2cc8e2c3ba29a4453c0dae065fd143886ee
Anthony Serrato <anthony17serrato@gmail.com>
2024-04-23 22:03:48 -0700

    Resolve build issues & update run configurations

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import kanbanboard.composeapp.generated.resources.Res
 import kanbanboard.composeapp.generated.resources.compose_multiplatform
 
